7a1cb4105675ee864318e8117c8074440b97727c
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2022-08-31 15:13:24 +0200

    RM API: support introspection triggers
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- apps/astarte_realm_management_api/mix.exs
+++ apps/astarte_realm_management_api/mix.exs
@@ -74,7 +74,9 @@ defmodule Astarte.RealmManagement.API.Mixfile do
 
   defp astarte_required_modules(_) do
     [
-      {:astarte_core, github: "astarte-platform/astarte_core"},
+      # github: "astarte-platform/astarte_core"},
+      {:astarte_core,
+       github: "Annopaolo/astarte_core", branch: "introspection-triggers-to-device"},
       {:astarte_rpc, github: "astarte-platform/astarte_rpc"}
     ]
   end
