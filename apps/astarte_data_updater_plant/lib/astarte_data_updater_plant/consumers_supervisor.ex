fda9479ca2e9341b6e13677418301e460007446c
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2024-02-01 12:51:30 +0100

    Introduce ExRabbitPool for consumers
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- apps/astarte_data_updater_plant/lib/astarte_data_updater_plant/consumers_supervisor.ex
+++ apps/astarte_data_updater_plant/lib/astarte_data_updater_plant/consumers_supervisor.ex
@@ -33,6 +33,9 @@ defmodule Astarte.DataUpdaterPlant.ConsumersSupervisor do
 
     children = [
       {Registry, [keys: :unique, name: Registry.AMQPDataConsumer]},
+      {ExRabbitPool.PoolSupervisor,
+       rabbitmq_config: Config.amqp_consumer_options!(),
+       connection_pools: [Config.amqp_consumer_pool_config!()]},
       {AMQPDataConsumer.ConnectionManager, amqp_opts: Config.amqp_consumer_options!()},
       AMQPDataConsumer.Supervisor
     ]
